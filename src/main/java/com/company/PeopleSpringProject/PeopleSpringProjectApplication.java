2661fcdd651fe59667bc9ab803154852e2a6381f
Anzelika Gorbaca <anzelika.spila@gmail.com>
2021-06-11 19:00:04 +0300

    first commit

---
+++ src/main/java/com/company/PeopleSpringProject/PeopleSpringProjectApplication.java
@@ -0,0 +1,15 @@
+package com.company.PeopleSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PeopleSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleSpringProjectApplication.class, args);
+    }
+
+}
